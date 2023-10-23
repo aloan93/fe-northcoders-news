744e6bf41d01f65ca152954854f28844f612bca8
aloan93 <a.loan93@gmail.com>
2023-10-23 11:18:07 +0100

    start made to task - early navbar functional with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+import Articles from "./components/Articles";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<Articles />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
