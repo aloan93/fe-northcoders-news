4d5062d7829938dd4c3ba9f77beb2316e79be21b
aloan93 <a.loan93@gmail.com>
2023-10-25 12:14:29 +0100

    refactored navigation buttons to only render when relevent

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,23 +1,26 @@
 export default function PageNav({ page, setPage, totalArticleCount }) {
   function pageUp(e) {
     e.preventDefault();
-    if (page * 10 < totalArticleCount) setPage(page + 1);
+    setPage(page + 1);
   }
 
   function pageDown(e) {
     e.preventDefault();
-    if (page > 1) setPage(page - 1);
+    setPage(page - 1);
   }
 
   return (
     <div className="page-navigation">
-      <button aria-label="back-page" onClick={pageDown}>
+      <button aria-label="back-page" onClick={pageDown} hidden={page === 1}>
         {"<"}
       </button>
       <p id="current-page" aria-label="current-page">
         {page}
       </p>
-      <button aria-label="forward-page" onClick={pageUp}>
+      <button
+        aria-label="forward-page"
+        onClick={pageUp}
+        hidden={totalArticleCount <= page * 10}>
         {">"}
       </button>
     </div>
