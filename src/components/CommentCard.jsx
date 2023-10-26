559113d730806f539b844dd211ab0f4d4e943432
aloan93 <a.loan93@gmail.com>
2023-10-26 12:16:52 +0100

    implemented delete button that only renders on users own comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import VotesManager from "./VotesManager";
 import api from "../api/api";
 import Loading from "./Loading";
+import Delete from "./Delete";
+import { UserContext } from "../contexts/User";
 
 export default function CommentCard({ comment }) {
   const [currComment, setCurrComment] = useState(comment);
   const [authorAvatar, setAuthorAvatar] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const timeConversion = new Date(currComment.created_at);
+  const { user } = useContext(UserContext);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -26,22 +30,34 @@ export default function CommentCard({ comment }) {
   if (isLoading) return <Loading />;
   return (
     <div className="comment-card">
-      <div className="comment-identity">
-        <img
-          className="comment-avatar"
-          src={authorAvatar}
-          alt={`user ${currComment.author}'s avatar`}
-        />
-        <p className="comment-author">{currComment.author}</p>
-      </div>
-      <p className="comment-timestamp">{timeConversion.toString()}</p>
-      <p>{currComment.body}</p>
-      <VotesManager
-        elementId={currComment.comment_id}
-        elementVotes={currComment.votes}
-        path="comments"
-        elementAuthor={currComment.author}
-      />
+      {!isDeleted ? (
+        <>
+          <div className="comment-identity">
+            <img
+              className="comment-avatar"
+              src={authorAvatar}
+              alt={`user ${currComment.author}'s avatar`}
+            />
+            <p className="comment-author">{currComment.author}</p>
+          </div>
+          <p className="comment-timestamp">{timeConversion.toString()}</p>
+          <p>{currComment.body}</p>
+          <VotesManager
+            elementId={currComment.comment_id}
+            elementVotes={currComment.votes}
+            path="comments"
+            elementAuthor={currComment.author}
+          />
+          {user === currComment.author && (
+            <Delete
+              comment_id={currComment.comment_id}
+              setIsDeleted={setIsDeleted}
+            />
+          )}
+        </>
+      ) : (
+        <p>Successfully Deleted</p>
+      )}
     </div>
   );
 }
